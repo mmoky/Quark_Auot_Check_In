e153ec1e39ae76b25c3166a69f07d69ec721e722
mmoky <momovpn18@gmail.com>
2026-03-07 14:30:08 +0800

    Handle growth info retrieval failure with exception
    Raise an exception for growth info retrieval failure.

--- checkIn_Quark.py
+++ checkIn_Quark.py
@@ -114,7 +114,6 @@ class Quark:
         '''
         log = ""
         # 每日领空间
-        print("❗️ user_data:", self, "\n")
         growth_info = self.get_growth_info()
         if growth_info:
             log += (
@@ -140,8 +139,8 @@ class Quark:
                 else:
                     log += f"❌ 签到异常: {sign_return}\n"
         else:
-            log += f"❌ 签到异常: 获取成长信息失败\n"
-            # raise Exception("❌ 签到异常: 获取成长信息失败")  # 适用于单账号情形，当 cookie 值失效后直接报错，方便通过 github action 的操作系统来进行提醒 如果你使用的是多账号签到的话，不要跟进此更新
+            # log += f"❌ 签到异常: 获取成长信息失败\n"
+            raise Exception("❌ 签到异常: 获取成长信息失败")  # 适用于单账号情形，当 cookie 值失效后直接报错，方便通过 github action 的操作系统来进行提醒 如果你使用的是多账号签到的话，不要跟进此更新
 
         return log
 
