e57185a7a9f25765c474b064e5a230630b637687
mmoky <momovpn18@gmail.com>
2026-03-07 12:55:58 +0800

    Add debug print statement for user data

--- checkIn_Quark.py
+++ checkIn_Quark.py
@@ -114,6 +114,7 @@ class Quark:
         '''
         log = ""
         # 每日领空间
+        print("❗️ user_data:", self, "\n")
         growth_info = self.get_growth_info()
         if growth_info:
             log += (
@@ -139,8 +140,8 @@ class Quark:
                 else:
                     log += f"❌ 签到异常: {sign_return}\n"
         else:
-            # log += f"❌ 签到异常: 获取成长信息失败\n"
-            raise Exception("❌ 签到异常: 获取成长信息失败")  # 适用于单账号情形，当 cookie 值失效后直接报错，方便通过 github action 的操作系统来进行提醒 如果你使用的是多账号签到的话，不要跟进此更新
+            log += f"❌ 签到异常: 获取成长信息失败\n"
+            # raise Exception("❌ 签到异常: 获取成长信息失败")  # 适用于单账号情形，当 cookie 值失效后直接报错，方便通过 github action 的操作系统来进行提醒 如果你使用的是多账号签到的话，不要跟进此更新
 
         return log
 
@@ -163,7 +164,7 @@ def main():
         for a in cookie_quark[i].replace(" ", "").split(';'):
             if not a == '':
                 user_data.update({a[0:a.index('=')]: a[a.index('=') + 1:]})
-        print("❗️ user_data:", user_data)
+        # print(user_data)
         # 开始任务
         log = f"🙍🏻‍♂️ 第{i + 1}个账号"
         msg += log
